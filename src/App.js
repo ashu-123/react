6fe84cacdfd239fa3e4c9b81cbb452b396d14e42
Ashutosh <Ashutosh>
2024-09-07 17:03:15 +0530

    consume conf user rest api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default App;
+    this.state = {
+      items: [],
+      DataisLoaded: false
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      "http://localhost:8080/api/v1/user/hello")
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          items: json,
+          DataisLoaded: true
+        });
+      })
+  }
+
+  render() {
+    const {DataisLoaded, items} = this.state;
+    if(!DataisLoaded) return <div>
+      <h1>Please wait..</h1>
+    </div>
+
+    return (
+      <div className="App">
+        <h1>Fetch data from an api in react</h1>
+        <ol key= {items.id} >
+          Full Name : { items.name }
+        </ol>
+      </div>
+    );
+  }
+}
